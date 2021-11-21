5b99ce3b4ec43e247740f1f36a12fb9d339797c4
Devin Stewart <49423028+Bigshmow@users.noreply.github.com>
2021-11-21 14:31:18 -0700

    react hook forms basics for simple login

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -12,16 +12,18 @@ export const Login = () => {
   console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+    <div className="d-flex justify-content-center align-items-center p-3">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
 
-      <input type="submit" />
-    </form>
+        <button className="btn btn-primary" type="submit" />
+      </form>
+    </div>
   );
 };
