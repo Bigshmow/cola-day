7793eabf599254f55c2b56050034e282dd1999c8
Devin Stewart <49423028+Bigshmow@users.noreply.github.com>
2021-11-22 16:52:35 -0700

    password and json methods

--- backend/src/collections/User.ts
+++ backend/src/collections/User.ts
@@ -1,4 +1,5 @@
 import { Document, model, Schema as _Schema } from "mongoose";
+import { hashSync, compareSync } from "bcrypt";
 
 const Schema = new _Schema(
   {
@@ -12,6 +13,24 @@ const Schema = new _Schema(
     timestamps: true,
   }
 );
+
+Schema.methods = {
+  setPassword(password: string) {
+    this.set("password", hashSync(password, 10));
+  },
+  verifyPassword(password: string) {
+    return compareSync(password, this.get("password"));
+  },
+  async toJSON_Web() {
+    return {
+      firstName: this.get("firstName"),
+      lastName: this.get("lastName"),
+      email: this.get("email"),
+      _id: this._id.toString(),
+      created_at: (this.get("createdAt") / 1000) | 0,
+    };
+  },
+};
 
 const User = model<Document & any>("User", Schema);
 export default User;
