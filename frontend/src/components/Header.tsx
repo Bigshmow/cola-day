9276da3d11ff2c8eb98d5785368d33a7c43d9486
Devin Stewart <49423028+Bigshmow@users.noreply.github.com>
2021-11-23 14:13:47 -0700

    header for starters with logout

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,53 @@
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { logoutUser } from "../store/user";
+
+export const Header = () => {
+  const user = useSelector(({ user }: any) => user.data);
+
+  const dispatch = useDispatch();
+
+  const logoutUserDispatch = useCallback(
+    () => dispatch(logoutUser()),
+    [dispatch]
+  );
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container d-flex align-items-center">
+        <a className="navbar-brand" href="/">
+          <img src="/apple-touch-icon.png" alt="logo" width="50" height="50" />
+          <span className="p-3">Cola Day</span>
+        </a>
+        <div className="ms-auto d-flex justify-content-between">
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {<FontAwesomeIcon icon={faUserCircle} size="3x" />}
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <span className="dropdown-item-text text-uppercase">
+                  {user.firstName}
+                </span>
+              </li>
+              <li>
+                <p onClick={logoutUserDispatch} className="dropdown-item">
+                  Log Out
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
