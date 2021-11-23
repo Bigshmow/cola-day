a2da80cb1e960537bd189f0591051eaad8e392dd
Devin Stewart <49423028+Bigshmow@users.noreply.github.com>
2021-11-23 14:13:25 -0700

    trim the fluff

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -2,10 +2,8 @@ import { LoginForm } from "../components/forms/Login";
 
 export const LoginView = () => {
   return (
-    <div className="d-flex flex-column align-items-center p-3">
-      <div className="p-5">
-        <h1>Welcome to Cola Day</h1>
-      </div>
+    <div className="p-5 mt-auto mb-auto d-flex flex-column align-items-center">
+      <h1 className="m-5 text-center">Login</h1>
       <LoginForm />
     </div>
   );
