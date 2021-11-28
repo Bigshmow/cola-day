7ef37c2f3086da0723b8755daf8c277fa7971280
Devin Stewart <49423028+Bigshmow@users.noreply.github.com>
2021-11-28 12:37:30 -0700

    client establish headers with sesh token

--- frontend/src/graphql/index.js
+++ frontend/src/graphql/index.js
@@ -1,7 +1,31 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { URL_API } from '../core/constants';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { URL_API } from "../core/constants";
+import { setContext } from "@apollo/client/link/context";
+
+const link = new HttpLink({ uri: `${URL_API}/ql` });
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = sessionStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 export const client = new ApolloClient({
-    uri: `${URL_API}/ql`,
-    cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  // uri: `${URL_API}/ql`,
+  link: authLink.concat(link),
+  defaultOptions: {
+    query: {
+      fetchPolicy: "no-cache",
+    },
+    mutate: {
+      fetchPolicy: "no-cache",
+    },
+  },
 });
